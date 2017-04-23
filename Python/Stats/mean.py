39c0fbefc865556a771e3323eb10bde6d6c71e84
Nicolas DUPONT <nicolas.dupont@outlook.com>
2017-04-23 22:03:55 +0200

    Update mean.py

--- Python/Stats/mean.py
+++ Python/Stats/mean.py
@@ -10,13 +10,29 @@
 
 # compute the mean
 
+#Array
+def calc_mean(arr):
+    l = len(arr)
+    for i in range(1,l):
+        s += arr[i] 
+    mean = s/l
+    return mean
+
+#series
 def calc_mean(serie):
     vals = serie.values
     return sum(vals)/len(vals)
 
-
+# numpy
 import numpy as np
 
 def calc_mean(nparray):
     mean = np.mean(nparray)
+    return mean
+  
+# Pandas 
+import pandas as pd
+
+def calc_mean(dfs):
+    mean = dfs.mean
     return mean
