723425ea7810bfb0650bad155ba95ba054a633a7
Nicolas DUPONT <nicolas.dupont@outlook.com>
2018-02-02 12:08:43 +0100

    Create ecdf.py

---
+++ Python/DataAnalysis/ecdf.py
@@ -0,0 +1,29 @@
+"""
+------------------------------------
+ Creation date : 02/02/2018  (fr)
+ Last update :   02/02/2018
+ Author(s) : Nicolas DUPONT
+ Contributor(s) :
+ Tested on Python 3.6
+------------------------------------
+"""
+
+#plot ecdf
+def PlotEcdf(data,title='',xlabel=''):
+    # Empirical cumulative distribution functions (ECDF)
+    # Compute ECDF for a one-dimensional array of measurements and plot it.
+    data = data.dropna()
+    n = len(data)
+    x = np.sort(data)
+    y = np.arange(1, len(x)+1) / len(x)
+    title = title+' Cumulative Distribution'
+    plt.plot(x, y, marker='.')
+    plt.margins(0)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel('ECDF (% of oberservation)')
+    plt.ylim(0,1)
+    
+
+#PlotEcdf(df[column],title='ColumnName',xlabel='$')
+#plt.show()
